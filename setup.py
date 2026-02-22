cf8908e5ea1f1df7725e8cf29b1ade98ebb31a45
Avez <avezmulani786@gmail.com>
2026-02-22 13:49:46 +0530

    project structure is set and ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Avez Mulani",
+    author_email="avezmulani786@gmail.com",
+    packages=find_packages()
+)
